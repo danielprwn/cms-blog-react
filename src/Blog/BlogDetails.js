22bef0b437d99d3d0954dcf6b3c072e0226d505d
danielprwn <danielprwn@gmail.com>
2022-09-16 12:09:43 +0200

    add files

---
+++ src/Blog/BlogDetails.js
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from "react";
+import { createClient } from "contentful";
+import { Link, useParams } from "react-router-dom";
+import ReactMarkdown from "react-markdown";
+
+const BlogDetails = () => {
+  const [detailsBlogPost, setDetailsBlogPost] = useState([]);
+
+  let { id } = useParams();
+
+  //Contenful API connection
+  const client = createClient({
+    space: "bypzioyfiiik",
+    accessToken: "0oX6CBN8J6imolLlvvixUNYydgJMagwaudIzE5DENTM",
+  });
+
+  useEffect(() => {
+    const getEntryById = async () => {
+      try {
+        await client.getEntry(id).then((entries) => {
+          setDetailsBlogPost(entries);
+        });
+      } catch (error) {
+        console.log(`Houston, we have a problem - Error ${error}`);
+      }
+    };
+    getEntryById();
+  }, [id]);
+
+  return (
+    <div id="layout" className="pure-g">
+      <div className="content pure-u-1 pure-u-md-3-4">
+        <div>
+          <div className="posts">
+            <Link to="/blogList" className="content-subhead">
+              ↩ Return to Blog Posts!
+            </Link>
+
+            <section className="post">
+              <header className="post-header">
+                <img
+                  src={detailsBlogPost?.fields?.blogImage?.fields?.file?.url}
+                  title=""
+                  alt={detailsBlogPost?.fields?.title}
+                  width="100%"
+                  height="100%"
+                />
+                <h2 className="post-title pt-3">
+                  {detailsBlogPost?.fields?.title}
+                </h2>
+                <p className="post-meta">
+                  Author Name ➡{" "}
+                  <a href="https://reactjs.org/" className="post-author">
+                    {detailsBlogPost?.fields?.blogAuthor}
+                  </a>{" "}
+                  Date: <span></span>
+                  <small>{detailsBlogPost?.fields?.createdDate}</small>
+                </p>
+              </header>
+              <div className="post-description">
+                <ReactMarkdown
+                  children={detailsBlogPost?.fields?.postContent}
+                />
+              </div>
+            </section>
+          </div>
+          <div className="footer">
+            <div className="pure-menu pure-menu-horizontal">
+              <div className="pure-menu pure-menu-horizontal">
+                <div className="pure-menu-item">
+                  <small>&copy; Copyright 2022 - Prwn</small>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogDetails;
---
+++ src/Blog/BlogList.js
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+import { createClient } from "contentful";
+import { Link } from "react-router-dom";
+
+const BlogList = () => {
+  const [blogPosts, setBlogPosts] = useState([]);
+
+  //Contenful API connection
+  const client = createClient({
+    space: "bypzioyfiiik",
+    accessToken: "0oX6CBN8J6imolLlvvixUNYydgJMagwaudIzE5DENTM",
+  });
+
+  useEffect(() => {
+    const getAllEntries = async () => {
+      try {
+        await client.getEntries().then((entries) => {
+          setBlogPosts(entries);
+        });
+      } catch (error) {
+        console.log(`Houston, we have a problem - Error ${error}`);
+      }
+    };
+    getAllEntries();
+  }, []);
+
+  return (
+    <div id="layout" className="pure-g">
+      <div className="content pure-u-1 pure-u-md-3-4">
+        <div>
+          <div className="posts">
+            <h1 className="content-subhead">React Contentful Blog</h1>
+
+            {blogPosts?.items?.map((post) => (
+              <section className="post" key={post.sys.id}>
+                <header className="post-header">
+                  <img
+                    src={post.fields.blogImage.fields.file.url}
+                    title=""
+                    alt={post.fields.title}
+                    width="100%"
+                    height="100%"
+                  />
+                  <h2 className="post-title pt-3">{post.fields.title}</h2>
+                  <p className="post-meta">
+                    Author Name ➡{" "}
+                    <a href="https://reactjs.org/" className="post-author">
+                      {post.fields.blogAuthor}
+                    </a>{" "}
+                    Date: <span></span>
+                    <small>{post.fields.createdDate}</small>
+                  </p>
+                </header>
+                <div className="post-description">
+                  <p>{post.fields.blogSummary}</p>
+                  <Link
+                    to={`/blogDetails/${post.sys.id}`}
+                    className="button button1"
+                  >
+                    Read More...
+                  </Link>
+                </div>
+              </section>
+            ))}
+          </div>
+
+          <div className="footer">
+            <div className="pure-menu pure-menu-horizontal">
+              <div className="pure-menu-item">
+                <small>&copy; Copyright 2022 - Prwn</small>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogList;
